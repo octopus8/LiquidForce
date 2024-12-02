533f057612839bb5d5da8ff0083f5ddb07f5ea8f
Brian A. Lee <brian.lee@metastreet.com>
2024-12-01 16:54:47 -1000

    Added start position.

--- Assets/LiquidForce/Startup.cs
+++ Assets/LiquidForce/Startup.cs
@@ -7,6 +7,26 @@ namespace LiquidForce
 {
     public class Startup : MonoBehaviour
     {
+        [SerializeField]
+        protected GameObject playerGameObject;
+
+        [SerializeField]
+        protected Transform startTransform;
+        
+        [SerializeField]
+        protected GameObject logConsole;
+
+        [SerializeField]
+        protected bool showLogConsole = true;
+        
+        private bool isPlayerInited = false;
+        
+
+        private void Awake()
+        {
+            logConsole.gameObject.SetActive(showLogConsole);
+        }
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
@@ -15,6 +35,21 @@ namespace LiquidForce
 #else
             StartCoroutine(FadeInDelayed());
 #endif
+        }
+
+        private int fixedFrameCount = 0;
+        
+        private void FixedUpdate()
+        {
+            if (isPlayerInited || ++fixedFrameCount < 2)
+            {
+                return;
+            }
+            
+            playerGameObject.transform.SetPositionAndRotation(startTransform.position, startTransform.rotation);
+            logConsole.transform.SetPositionAndRotation(playerGameObject.transform.position, playerGameObject.transform.rotation);
+            isPlayerInited = true;
+            
         }
 
 
@@ -32,7 +67,6 @@ namespace LiquidForce
             yield return new WaitForSeconds(1);
             Application.Instance.cameraFader.FadeCameraIn();
             Application.Instance.OnXRChange -= OnXRChange;
-            Destroy(gameObject);
         }
     }
     
