3bf8d445e632de94b5b264cd5317231274d3d06d
Brian A. Lee <octopus8creations@gmail.com>
2025-08-10 14:51:28 -1000

    Making good progress on laying out a trail.

--- Assets/[LiquidForce]/TileTreePositioner.cs
+++ Assets/[LiquidForce]/TileTreePositioner.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 public class TileTreePositioner : MonoBehaviour
 {
     [SerializeField]
-    private TreeTile[] tiles;
+    private TileRun[] tileRuns;
 
     [SerializeField] private float tileSize = 5f;
     
     [Serializable]
-    class TreeTile
+    class TileRun
     {
         public string name;
         public Axis axis = Axis.Z;
-        public GameObject[] tiles;
+        public GameObject parentGameObject;
     }
     
     [Serializable]
@@ -31,47 +31,42 @@ public class TileTreePositioner : MonoBehaviour
     /// </summary>
     public void PositionTiles()
     {
-        if (tiles == null || tiles.Length == 0)
+        if (tileRuns == null || tileRuns.Length == 0)
         {
             Debug.LogWarning("No tiles to position.");
             return;
         }
 
-        foreach (var tile in tiles)
+        foreach (var tileRun in tileRuns)
         {
-            if (tile.tiles == null || tile.tiles.Length == 0)
+            for (int i = 1; i < tileRun.parentGameObject.transform.childCount; i++)
             {
-                Debug.LogWarning("No tiles in this group to position.");
-                continue;
-            }
-
-            for (int i = 1; i < tile.tiles.Length; i++)
-            {
+                Transform parentTile = tileRun.parentGameObject.transform.GetChild(i - 1);
                 Vector3 vec = new Vector3(0, 0, tileSize);
                 Quaternion rot = Quaternion.identity; 
-                switch (tile.axis)
+                switch (tileRun.axis)
                 {
                     case Axis.X:
                         vec = new Vector3(tileSize, 0, 0);
-                        rot = Quaternion.Euler(0, 0, tile.tiles[i - 1].transform.rotation.eulerAngles.x);
+                        rot = Quaternion.Euler(0, 0, -parentTile.rotation.eulerAngles.x);
                         break;
                     case Axis.Z:
                         vec = new Vector3(0, 0, tileSize);
-                        rot = Quaternion.Euler(tile.tiles[i - 1].transform.rotation.eulerAngles.x, 0, 0);
+                        rot = Quaternion.Euler(parentTile.rotation.eulerAngles.x, 0, 0);
                         break;
                     case Axis.NegativeX:
                         vec = new Vector3(-tileSize, 0, 0);
-                        rot = Quaternion.Euler(0, 0, tile.tiles[i - 1].transform.rotation.eulerAngles.x);
+                        rot = Quaternion.Euler(0, 0, parentTile.rotation.eulerAngles.x);
                         break;
                     case Axis.NegativeZ:
                         vec = new Vector3(0, 0, -tileSize);
-                        rot = Quaternion.Euler(tile.tiles[i - 1].transform.rotation.eulerAngles.x, 0, 0);
+                        rot = Quaternion.Euler(parentTile.rotation.eulerAngles.x, 0, 0);
                         break;
                 }
                 vec = rot * vec;
-                vec += tile.tiles[i - 1].transform.position;
+                vec += parentTile.position;
                 
-                tile.tiles[i].transform.position = vec;
+                tileRun.parentGameObject.transform.GetChild(i).position = vec;
             }
         }
         Debug.Log("Tiles positioned successfully.");
